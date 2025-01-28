05964a434c3f7c9c4b768cea1f85d939337e0089
cassiopeia001 <79790000+cassiopeia001@users.noreply.github.com>
2025-01-28 16:20:51 +0100

    updated

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const iconMenu= document.getElementById("menu-icon");
+const iconClose= document.getElementById("close-icon");
+const navbar= document.getElementById("navbar");
+const list= document.getElementById("list");
+const logo= document.getElementById("logo");
+const socials= document.getElementById("socials");
+const featuresContainer= document.getElementById("features-container");
+const featuresList= featuresContainer.querySelectorAll(':scope > div');
+const tabContainer= document.getElementById('tab-container');
+const tabList= tabContainer.querySelectorAll('button');
+const form= document.getElementById('form');
+const email= document.getElementById('email');
+const submitButton= document.getElementById('submit-btn');
+
+iconMenu.addEventListener('click', ()=>{
+    navbar.classList.add("fixed", "mt-0", "top-0", "left-0","w-full","h-full","z-20","bg-VeryDarkBlue","opacity-85","flex-col", "p-10");
+    navbar.classList.remove('mt-7');
+    iconMenu.classList.remove("md:block");
+    iconClose.classList.remove("hidden");
+    list.classList.remove("md:hidden");
+    socials.classList.add("flex");
+    socials.classList.remove("hidden");
+    logo.src="/images/logo-bookmark -light.svg";
+});
+iconClose.addEventListener('click', ()=>{
+    navbar.classList.remove("fixed", "top-0", "left-0","w-full","h-full","mt-0","z-20","bg-VeryDarkBlue","opacity-85","flex-col", "p-10");
+    navbar.classList.add('mt-7');
+    iconMenu.classList.add("md:block");
+    iconClose.classList.add("hidden");
+    list.classList.add("md:hidden");
+    socials.classList.remove("flex");
+    socials.classList.add("hidden");
+    logo.src="/images/logo-bookmark.svg";
+});
+
+function switchTab(evt,tab){
+    featuresList.forEach(feature=> {
+        feature.classList.add('hidden');
+    });
+    const currentTab= document.getElementById(tab);
+    currentTab.classList.remove('hidden');
+    currentTab.classList.add('flex');    
+     
+    tabList.forEach(btn=>{
+        btn.classList.add('after:opacity-0');
+        btn.classList.remove('after:opacity-100');
+    });
+    evt.currentTarget.classList.add('after:opacity-100');
+}
+submitButton.addEventListener("click", ()=> {
+    if (!email.checkValidity()) {
+
+        submitButton.classList.add("bg-white", "border-SoftRed", "text-SoftRed");
+    }
+    else{
+        submitButton.classList.remove("bg-white", "border-SoftRed", "text-SoftRed");
+    }
+})
+
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'SoftBlue': 'hsl(231, 69%, 60%)',
+        'SoftRed': 'hsl(0, 94%, 66%)',
+        'GrayishBlue': 'hsl(229, 8%, 60%)', 
+        'VeryDarkBlue': 'hsl(229, 31%, 21%)',
+      },
+      fontFamily: {
+        "rubik": ["Rubik", "sans-serif"],
+      },
+    },
+    screens: {
+      'md': { 'max': '768px' },
+      'sm': { 'max': '640px' },
+    },
+  },
+  plugins: [],
+}
+
